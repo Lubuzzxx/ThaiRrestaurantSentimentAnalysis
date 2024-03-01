105a8c6364d233adf1996cdc18b815c22b3ffa71
Lubuzzxx <141214557+Lubuzzxx@users.noreply.github.com>
2024-03-01 19:17:44 +0700

    userChooseModel.py

--- userChooseModel.py
+++ userChooseModel.py
@@ -8,6 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import os
 import re
+import time
 
 if __name__ == '__main__':
 
@@ -36,55 +37,94 @@ if __name__ == '__main__':
     def back():
         root.destroy()
         subprocess.run(['python', 'userChooseFile.py'])
+
+    def center_window(window):
+        window.update_idletasks()
+        width = window.winfo_width()
+        height = window.winfo_height()
+        x = (window.winfo_screenwidth() // 2) - (width // 2)
+        y = (window.winfo_screenheight() // 2) - (height // 2)
+        window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     
     predict_result = []
     def confirm_model():
-        class_result = ""
-        if select_model == "":
-            messagebox.showwarning("Warning", "โปรดเลือกอย่างน้อย 1 โมเดล")
-        else:
-            word_token_lines = ""
-            # with open('predictedResult.csv', 'w', newline='') as csv_file:
-            #     pass
-            with open("textprepare\word_tokens.txt", "r") as f:
-                word_token_lines = f.readlines()
-            # split_result = word_token_lines[0].split(',')
-            # file_name = split_result[0]
-            # data_file_path = split_result[1]
+        try:
+            class_result = ""
+            if select_model == "":
+                messagebox.showwarning("Warning", "โปรดเลือกอย่างน้อย 1 โมเดล")
+            else: 
+                popup_window = tk.Toplevel(root)
+                popup_window.title("Model is Processing กรุณาอย่าปิดหน้าต่างนี้")
+                popup_window.resizable(False, False)
+                popup_window.geometry("500x100")
+                center_window(popup_window)
+                label_tk = tk.Label(popup_window, font=('TH SarabunPSK', 14))
+                label_tk.pack(padx=20, pady=20)
+                label_tk.config(text="กำลังประมวลผล")
+                popup_window.update()
 
-            file_path_m = os.path.join("allModels", select_model)
-            m = pickle.load(open(file_path_m, 'rb'))
+                word_token_lines = ""
+                # with open('predictedResult.csv', 'w', newline='') as csv_file:
+                #     pass
+                with open("textprepare\word_tokens.txt", "r") as f:
+                    word_token_lines = f.readlines()
+                # split_result = word_token_lines[0].split(',')
+                # file_name = split_result[0]
+                # data_file_path = split_result[1]
 
-            file_path_cv = os.path.join("allModels", select_cv)
-            cv = pickle.load(open(file_path_cv, 'rb'))
-            vectorizer = CountVectorizer(vocabulary=cv)
+                file_path_m = os.path.join("allModels", select_model)
+                m = pickle.load(open(file_path_m, 'rb'))
 
-            file_path_text = os.path.join("textprepare", word_token_lines[0])
-            doc_tokens = pickle.load(open(file_path_text, 'rb'))
-            
-            df = pd.read_csv("data_file.csv", names=['text'],usecols = [0])  
-            df = df.dropna()    
-            for doc in doc_tokens:
-                data2 = vectorizer.transform(pd.Series(str(doc)))
-                dataVector2 = pd.DataFrame(data2.toarray(), columns=vectorizer.get_feature_names_out())
-                X2 = dataVector2.values[:,:]
-                my_predictions = m.predict(X2)
-                if str(my_predictions) == "['pos']":
-                    class_result = "positive"
-                elif str(my_predictions) == "['neg']":
-                    class_result = "negative"
-                elif str(my_predictions) == "['neu']":
-                    class_result = "neutral"
-                predict_result.append(class_result)
-            result_df = pd.DataFrame({'text': df['text'], 'class': predict_result})
-            result_df.to_excel('predictedResult.xlsx', index=False)
-            # with open('predictedResult.csv', 'a', newline='', encoding="utf-8") as csv_file:
-            #     csv_writer = csv.writer(csv_file)
-            #     write_row = df['text'][index],class_result
-            #     csv_writer.writerow(write_row)
-            messagebox.showinfo("","วิเคราะห์เสร็จเรียบร้อย")
-            root.destroy()
-            subprocess.run(['python', 'userResult.py'])
+                file_path_cv = os.path.join("allModels", select_cv)
+                cv = pickle.load(open(file_path_cv, 'rb'))
+                vectorizer = CountVectorizer(vocabulary=cv)
+
+                file_path_text = os.path.join("textprepare", word_token_lines[0])
+                doc_tokens = pickle.load(open(file_path_text, 'rb'))
+                
+                df = pd.read_csv("data_file.csv", names=['text'],usecols = [0])  
+                df = df.dropna()  
+                
+                counting_dot = 0
+                for doc in doc_tokens:
+                    data2 = vectorizer.transform(pd.Series(str(doc)))
+                    dataVector2 = pd.DataFrame(data2.toarray(), columns=vectorizer.get_feature_names_out())
+                    X2 = dataVector2.values[:,:]
+                    my_predictions = m.predict(X2)
+                    if str(my_predictions) == "['pos']":
+                        class_result = "positive"
+                    elif str(my_predictions) == "['neg']":
+                        class_result = "negative"
+                    elif str(my_predictions) == "['neu']":
+                        class_result = "neutral"
+                    predict_result.append(class_result)
+                    counting_dot += 1
+                    if counting_dot == 1:
+                        label_tk.config(text="กำลังประมวลผล")
+                        popup_window.update()
+                    elif counting_dot == 2:
+                        label_tk.config(text="กำลังประมวลผล.")
+                        popup_window.update()
+                    elif counting_dot == 3:
+                        label_tk.config(text="กำลังประมวลผล..")
+                        popup_window.update()
+                    elif counting_dot == 4:
+                        counting_dot = 0
+                        label_tk.config(text="กำลังประมวลผล...")
+                        popup_window.update()
+                    time.sleep(0.3)
+                result_df = pd.DataFrame({'text': df['text'], 'class': predict_result})
+                result_df.to_excel('predictedResult.xlsx', index=False)
+                # with open('predictedResult.csv', 'a', newline='', encoding="utf-8") as csv_file:
+                #     csv_writer = csv.writer(csv_file)
+                #     write_row = df['text'][index],class_result
+                #     csv_writer.writerow(write_row)
+                popup_window.destroy()
+                messagebox.showinfo("","วิเคราะห์เสร็จเรียบร้อย")
+                root.destroy()
+                subprocess.run(['python', 'userResult.py'])
+        except Exception as e:
+            tk.messagebox.showwarning("",f"{e}")
     
     root = tk.Tk()
     root.title("User Choose Model")
